50d44d1abfe9b1df6b76a985a832f2f54eb6c5ce
GillianY1 <48823988+GillianY1@users.noreply.github.com>
2024-03-21 14:09:20 +0800

    Add middleware function for api/posts route -#03-38

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,31 @@ const http = require('http');
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
-app.use((req, res, next)=>{
+/*app.use((req, res, next)=>{
   console.log('Request URL: ' + req.url);
   next();
 })
+*/
 
-app.use((req, res, next)=>{
-  res.send('Ciao , from express Gina');
+app.use("/api/posts",(req, res, next)=>{
+  const posts = [
+    {
+      id: '1',
+      title: 'First server-side post',
+      content: 'This is the first content coming from the server'
+    },
+    {
+      id: '2',
+      title: 'Second server-side post',
+      content: 'This is the second content coming from the server!'
+    }
+  ];
+
+  //res.send('Hello from express!');
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
 })
 
 module.exports = app;
