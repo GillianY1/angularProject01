6470f41ef996091829a6b6028086055f65a56e44
GillianY1 <48823988+GillianY1@users.noreply.github.com>
2024-03-22 16:34:35 +0800

    get posts from db -#03-55

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const Post = require('./models/post');
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb+srv://mindy:UrLFUJxz60MxwnSq@cluster0.v5msgya.mongodb.net/')
+mongoose.connect('mongodb+srv://mindy:XXXX@cluster0.v5msgya.mongodb.net/meanCourse?retryWrites=true&w=majority&appName=Cluster0')
 .then(()=>{
   console.log('Connected to database!');
 })
@@ -31,13 +31,13 @@ app.use((req, res, next)=>{ // next() is used to pass the request
 });
 
 // to handle post requests
-app.post("api/posts",(req, res, next)=>{
+app.post("/api/posts",(req, res, next)=>{
   const post = new Post({
     title: req.body.title,
     content: req.body.content
-  }); //req.body;
-
+  });
   console.log(post);
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
@@ -45,24 +45,26 @@ app.post("api/posts",(req, res, next)=>{
 
 
 app.use("/api/posts",(req, res, next)=>{
-  const posts = [
-    {
-      id: '1',
-      title: 'First server-side post',
-      content: 'This is the first content coming from the server'
-    },
-    {
-      id: '2',
-      title: 'Second server-side post',
-      content: 'This is the second content coming from the server!'
-    }
-  ];
+  Post.find()
+    .then((documents)=>{
+      console.log(documents);
+      res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: documents.map((doc)=>{ // map() is used to transform the array
+          return {
+            id: doc._id,
+            title: doc.title,
+            content: doc.content
+          };
+      })
+    })
+  })
+  .catch((err)=>{
+      console.log(err);
+ });
 
   //res.send('Hello from express!');
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
-  });
+
 })
 
 module.exports = app;
